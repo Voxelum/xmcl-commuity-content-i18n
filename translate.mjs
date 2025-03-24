8802c22120289a4ccd4ee18644d1c6d7877a6e4c
ci010 <cijhn@hotmail.com>
2025-03-24 14:39:45 +0800

    debug for translate

---
+++ translate.mjs
@@ -0,0 +1,111 @@
+const fs = require("fs");
+
+const dirs = fs.readdirSync("src");
+const allowedLocales = ["zh-cn"];
+
+export const chat = (messages) => {
+    const key = process.env.DEEPSEEK_API_KEY;
+    return fetch("https://api.deepseek.com/chat/completions", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${key}`,
+        },
+        body: JSON.stringify({
+            model: "deepseek-chat",
+            messages,
+        }),
+    }).then((resp) => resp.json());
+};
+
+async function translateByDS(descriptions) {
+    const resp = await chat([
+        {
+            role: "system",
+            content:
+                "You are an asistant of a Minecraft mod developer. You are asked to translate the mod description into different languages by locale code. Please do not add locale prefix to output. Users will input multiple description split by line feed. You should translate each description into the target locale and output them in the same order with \t spliting original text and translated text.",
+        },
+        {
+            role: "user",
+            content:
+                "Translate following into zh-CN:\nThis is an example\nThis is the second description!\nHello world~",
+        },
+        {
+            role: "assistant",
+            content: "This is an example\t这是一个例子\nThis is the second description!\t这是第二个简介！\nHello world~\t你好世界~",
+        },
+        {
+            role: "user",
+            content: `Translate following text into ${locale}:\n${descriptions.join("\n")}`,
+        },
+    ]);
+    if ("error" in resp) {
+        return resp;
+    }
+    let content = resp.choices[0].message.content;
+    if (content.startsWith('```' + locale)) {
+        content = content.substring(('```' + locale).length);
+        content = content.substring(0, content.length - 3);
+    }
+    if (content.startsWith(locale)) {
+        content = content.substring(locale.length);
+    }
+    return Object.fromEntries(content.split("\n").filter((l) => l.trim().length > 0).map(r => r.split("\t")));
+}
+
+async function getModrinthDescription(ids) {
+    // fetch projects by modrinth ids
+    const resp = await fetch("https://api.modrinth.com/api/v1/projects", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(ids),
+    });
+    const projects = await resp.json();
+    return projects.map((p) => [p.id, p.description]);
+}
+
+async function main() {
+    for (const dir of dirs) {
+        if (!allowedLocales.includes(dir)) {
+            continue;
+        }
+
+        const files = fs.readdirSync(`src/${dir}`);
+        for (const file of files) {
+            if (file.endsWith(".csv")) {
+                const csvFile = fs.readFileSync(`src/${dir}/${file}`, "utf-8");
+                const lines = csvFile.split("\n");
+                const csvContentLines = lines.map((l) => l.split(","));
+                const header = csvContentLines.shift(); // remove the header
+
+                // bat handle 32 lines
+                for (let i = 0; i < csvContentLines.length; i += 32) {
+                    const chunk = csvContentLines.slice(i, i + 32)
+                    // filter only for modrinth and no description
+                    const descriptions = chunk.filter((row) => !!row[1] && !row[3])
+
+                    const results = await getModrinthDescription(descriptions.map((row) => row[1]));
+
+                    const dict = Object.fromEntries(results)
+
+                    const resolvedDescriptions = descriptions.map((row) => ({ row, descrption: dict[row[1]] }))
+
+                    const translated = await translateByDS(resolvedDescriptions.map((resolved) => resolved.row[1]));
+
+                    console.log(translated)
+
+                    for (const { row, descrption } of resolvedDescriptions) {
+                        row[3] = translated[descrption] || descrption
+                    }
+                    break
+                }
+
+                fs.writeFileSync(`src/${dir}/${file}`, [header, ...csvContentLines.map((l) => l.join(",")).join("\n")].join("\n"));
+            }
+        }
+    }
+}
+
+main()
